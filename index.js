3ddb3a9d1226a47d91e81d7625fd0f7d128319ab
mrlaboratory <mrlaboratory23@gmail.com>
2023-05-20 10:32:07 +0600

    addede new route

--- index.js
+++ index.js
@@ -88,6 +88,11 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/test',async (req,res)=> {
+            const result = await notesCollection.find().toArray()
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
